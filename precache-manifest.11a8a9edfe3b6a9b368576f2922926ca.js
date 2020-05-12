641ff737fabf99bbcdb4e2f2e904b575ad5682f3
dfawole66@gmail.com <dfawole66@gmail.com>
2020-05-12 16:40:22 +0100

    Updates

---
+++ precache-manifest.11a8a9edfe3b6a9b368576f2922926ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cdfbfe52f152a681ab2dc22f9a5f35f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2503d6ff1c03df83dad0",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "926e89db33d243287268",
+    "url": "/static/css/main.08caf824.chunk.css"
+  },
+  {
+    "revision": "2503d6ff1c03df83dad0",
+    "url": "/static/js/2.9c2ceb72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9c2ceb72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "926e89db33d243287268",
+    "url": "/static/js/main.e16d6388.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
